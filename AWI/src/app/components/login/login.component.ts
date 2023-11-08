ddfdb167f1eb18fe23e12c0eb6553d9859118061
margotgoudard <goudard.margot@hotmail.com>
2023-11-08 14:22:08 +0100

    LoginComponent

---
+++ AWI/src/app/components/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  errorMessage: String = '';
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+
+      this.authService.login(username, password).subscribe(
+          (response) => {
+            // Connexion réussie, stockez le token JWT côté client (exemple hypothétique)
+            const jwtToken = response.token;
+        
+            // Vous pouvez stocker le token JWT dans le local storage ou un cookie
+            localStorage.setItem('jwtToken', jwtToken);
+        
+            // Redirection vers la page d'accueil ou une autre action souhaitée
+            // Exemple de redirection vers la page d'accueil :
+            this.router.navigate(['/accueil']);
+        
+            // Autres actions à exécuter après une connexion réussie
+          },
+          (error) => {
+            // Erreurs d'authentification
+            const errorMessage = "Échec de la connexion. Vérifiez vos informations d'identification.";
+        
+            // Affichez un message d'erreur à l'utilisateur, par exemple en utilisant une variable d'erreur dans le template HTML
+            this.errorMessage = errorMessage;
+        
+            // Vous pouvez également réinitialiser le formulaire si vous le souhaitez
+            this.loginForm.reset();
+        
+            // Autres actions à exécuter en cas d'échec de connexion
+          }
+        );
+    }
+  }
+}
---
+++ AWI/src/app/services/auth.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'URL_DE_VOTRE_API'; // Remplacez par l'URL de votre API.
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    // Effectuez une requête HTTP vers votre API backend pour vérifier les identifiants et obtenir un token JWT.
+    const loginData = { username, password };
+    return this.http.post(`${this.apiUrl}/login`, loginData).pipe(
+      catchError((error: any) => {
+        // En cas d'erreur, renvoyez un message d'erreur.
+        let errorMessage = 'Erreur lors de la connexion.';
+        
+        if (error.status === 401) {
+          errorMessage = 'Identifiants invalides. Veuillez réessayer.';
+        } else if (error.status === 500) {
+          errorMessage = 'Une erreur interne du serveur s\'est produite. Veuillez réessayer plus tard.';
+        }
+        
+        return throwError(errorMessage);
+      }),
+      tap((response: any) => {
+        // Stockez le token JWT dans le stockage local en cas de réussite.
+        localStorage.setItem('jwtToken', response.token);
+      })
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    // Effectuez une requête HTTP vers votre API backend pour enregistrer un nouvel utilisateur.
+    // Par exemple, utilisez http.post() pour envoyer les données d'inscription au backend.
+    const userData = { username, email, password };
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+
+  logout(): void {
+    // Supprimez le token JWT du stockage local.
+    localStorage.removeItem('jwtToken');
+  }
+
+  getJwtToken(): string | null {
+    return localStorage.getItem('jwtToken');
+  }
+
+  isUserLoggedIn(): boolean {
+    // Vérifiez la présence du token JWT dans le stockage local.
+    const token = localStorage.getItem('jwtToken');
+    return !!token; // Renvoie true si le token JWT est présent, sinon false.
+  }
+}
